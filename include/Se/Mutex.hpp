cfbbe2458536813c200536c983ffd2adf5fdb04c
krombel <krombel@tuta.io>
2024-09-21 10:15:45 +0300

    Mutex.hpp cleanup

--- include/Se/Mutex.hpp
+++ include/Se/Mutex.hpp
@@ -15,31 +15,6 @@
 
 namespace Se {
 
-// /// Wrapper for the C++ std::mutex.
-// using Mutex = std::mutex;
-
-// /// Wrapper for the C++ std::recursive_mutex.
-// using RecursiveMutex = std::recursive_mutex;
-
-// // /// Wrapper for the C++ std::condition_variable.
-// // using Signal = std::condition_variable;
-
-// // /// Wrapper for the C++ std::thread.
-// // using Thread = std::thread;
-
-// /// Wrapper for the C++ std::thread::id.
-// using ThreadId = std::thread::id;
-
-// /// Wrapper for the C++ std::unique_lock<std::mutex>.
-// using Lock = std::unique_lock<Mutex>;
-
-// /// Wrapper for the C++ std::unique_lock<std::recursive_mutex>.
-// using RecursiveLock = std::unique_lock<RecursiveMutex>;
-
-// using LockGuard = std::lock_guard<Mutex>;
-// using RecursiveLockGuard = std::lock_guard<RecursiveMutex>;
-
-
 #if _WIN32
     namespace Detail
 {
