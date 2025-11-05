e7facc70fd7d0f2382b847691ae89fc8d0003b3b
ikrombel <145226899+ikrombel@users.noreply.github.com>
2025-11-05 22:01:05 +0200

    [Se] add ModuleManager - add method: void Audio::Init() for update constuctor where module register in Module manager - implement base attributes for SeArc

--- include/Se/Extension.hpp
+++ include/Se/Extension.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Se/Console.hpp>
+#include <SeArc/ArchiveSerialization.hpp>
 
 #include <functional>
 #include <memory>
@@ -24,14 +25,51 @@ public:
         name_ = name;
     }
 
-    void SetModule(Module<Extension>* module) 
+    virtual ~Extension() //= default;
     {
-        module_ = module;
+        SE_LOG_DEBUG("Extension {} destroyed", name_);
     }
 
-    private:
-        Module<Extension>* module_ = nullptr;
-        std::string name_ = "";
+    // void SetModule(Module<Extension>* module) 
+    // {
+    //     module_ = module;
+    // }
+
+    /// Get extension name.
+    const std::string& GetName() const { return name_; }
+    /// Check whether the extension is enabled.
+    bool IsEnabled() const { return enabled_; }
+    /// Set enabled state.
+    void SetEnabled(bool enable) { enabled_ = enable; }
+
+    // bool CheckDependencies()
+    // {
+    //     for (auto& dep : dependencies_)
+    //     {
+    //         if (!dep->IsEnabled())
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
+    virtual void SerializeInBlock(Archive& archive)
+    {
+        SerializeValue(archive, "Name", name_);
+        SerializeValue(archive, "Enabled", enabled_);
+        SerializeValue(archive, "Dependencies", dependencies_);
+
+    }
+    
+
+private:
+    //Module<Extension>* module_ = nullptr;
+    /// 
+    std::string name_ = "";
+    /// 
+    bool enabled_ = true;
+    /// required dependies
+    std::vector<std::string> dependencies_;
+
 };
 
 template<typename T>
@@ -41,6 +79,13 @@ public:
     Module(const std::string& extBaseName) {
         extBaseName_ = extBaseName;
     }
+
+    ~Module() {
+        extensionFactory_.clear();
+        SE_LOG_DEBUG("Module {} destroyed", extBaseName_);
+    }
+
+    virtual void Init() {}
 
     template<typename U0, typename U1>
     struct Reg {
@@ -80,14 +125,54 @@ private:
     }
 };
 
+class ModuleManager
+{
+public:
+    template<typename T>
+    T* GetModule() 
+    {
+        std::string name = typeid(T).name();
+        auto it = modules_.find(name);
+        if (it != modules_.end()) {
+            return reinterpret_cast<T*>(it->second);
+        }
+        SE_LOG_ERROR("Module {} not found", name);
+        return nullptr;
+    }
 
+    template<typename T>
+    void AddModule() 
+    {
+        modules_[typeid(T).name()] = reinterpret_cast<Module<Extension>*>(T::Get());
+    }
+
+    ~ModuleManager() 
+    {
+        Destroy();
+    }
+
+    void Destroy() 
+    {
+        for (auto it = modules_.rbegin(); it != modules_.rend(); ++it) {
+            delete it->second;
+        }
+        modules_.clear();
+    }
+    
+protected:
+    std::map<std::string, Module<Extension>*> modules_;
+};
+
+// inline void SerializeValue(Archive& archive, const char* name, StringHash& value) {
+//     archive.Serialize(name, value.MutableValue());
+// }
 
 } // namespace Se
 
 
 #define SE_MODULE(moduleName) \
 public: \
-    moduleName() : Module(#moduleName) {} \
+    moduleName() : Module(#moduleName) { Init(); } \
     static moduleName* Get() \
     { \
         static moduleName* ptr = nullptr; \
